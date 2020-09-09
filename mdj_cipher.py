57168913c8b97ecb125a19ccb84c3b4cc50debf8
DanteMaxFlo <dante.flores@distribuidoradelaoferta.com.mx>
2020-09-09 18:04:31 -0500

    work in progress

--- mdj_cipher.py
+++ mdj_cipher.py
@@ -1,8 +1,39 @@
-text_plano = "Lorem ipsum dolor si"
-clave = "Lorem ip"
-code = "utf8"
-clave_bin = bytes(clave, code)
 
+def main():
+    texto_plano = "Lorem ipsum dolor si"
+    clave = "t amet orci aliquam."
+    code = "utf8"
+    clave_bin = bytes(clave, code)
+    desplazamiento = extractKBits(clave_bin[-1],3,1) + 1
+
+    # Dividir texto plano
+    bloques_txt = dividir_bloques(texto_plano, 10)
+
+    print('BLOQUE A CIFRAR:\t', bloques_txt[0])
+
+    bloques_txt[0] = cifrado_cesar(bloques_txt[0], desplazamiento)
+    print('CESAR:\t\t\t',bloques_txt[0])
+
+    clave = mover_bytes(clave)
+    print('DIFUSION LLAVE:\t\t', clave)
+
+    subbloques = dividir_bloques(bloques_txt[0], 2)
+    print('SubBloques:\t', subbloques)
+
+    difusion_subbloques = difusion_filas(subbloques)
+    print('DIFUSION 2:\t\t', difusion_subbloques)
+
+
+def difusion_filas(subbloques):
+    result = ''
+    
+    for i in range(2):
+        for b in subbloques:
+            result += b[i]
+
+    return result
+    
+    
 
 def extractKBits(num,k,p): 
   
@@ -22,6 +53,7 @@ def extractKBits(num,k,p):
      return int(kBitSubStr,2)
 
 def cifrado_cesar(tp, desplazamiento):
+    
     cesar = ''
     for char in tp:
         aux = ord(char) + desplazamiento
@@ -40,6 +72,10 @@ def mover_bytes(llave):
     llave = mitad2 + mitad1
     return llave
 
+def dividir_bloques(texto, n):
+    return [texto[i:i+n] for i in range(0, len(texto), n)]
+
+"""
 desplazamiento = extractKBits(clave_bin[-1],3,1) + 1
 print(desplazamiento)
 s = "9"
@@ -49,3 +85,10 @@ cesar = cifrado_cesar(text_plano,desplazamiento)
 print(cifrado_cesar(cesar,-1*desplazamiento))
 
 print(mover_bytes(clave_bin))
+"""
+
+if __name__ == "__main__":
+    main()
+
+
+
