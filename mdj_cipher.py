0f1cdf631cb6f9100fc8a4a0be11b8b8f6a3f3b6
97joaquinhr <97joaquinhr@gmail.com>
2020-09-11 12:39:09 -0500

    Missing loops

--- mdj_cipher.py
+++ mdj_cipher.py
@@ -3,61 +3,69 @@ import operator
 TAMANO_BLOQUE = 8
 CODE = "utf8"
 N_FILAS = 2
-DIF_LLAVE_BYTES = 3 
+DIF_LLAVE_BYTES = 3
 
 def main():
     texto_plano = "Hola amigos jej hol"
     llave = "t amet orci aliquam."
-
+    len_original = len(texto_plano)
     llave = normalizar_llave(llave,TAMANO_BLOQUE)
-    
+    texto_plano = agregar_x(texto_plano)  
     # Dividir texto plano
     bloques_txt = dividir_bloques(texto_plano, TAMANO_BLOQUE)
     bloques_bytes = bloques_a_bytes(bloques_txt)
     print(bloques_bytes)
+    for i in range(len(bloques_bytes)):
     # ------ CIFRADO ------
-    print('BLOQUE A CIFRAR:\t', bloques_bytes[0])
-    # Confusion 1: Cesar
-    bloques_bytes[0] = cifrado_cesar(bloques_bytes[0], get_desplazamiento(llave))
-    print('CESAR:\t\t\t',bloques_bytes[0])
-    # Difusion 1: Mover bits en llave
-    print('Llave:\t\t\t',llave)
-    llave = mover_bytes(llave)
-    print('DIFUSION LLAVE:\t\t', llave)
+        print('BLOQUE A CIFRAR:\t', bloques_bytes[i])
+        # Confusion 1: Cesar
+        bloques_bytes[i] = cifrado_cesar(bloques_bytes[i], get_desplazamiento(llave))
+        print('CESAR:\t\t\t',bloques_bytes[i])
+        # Difusion 1: Mover bits en llave
+        print('Llave:\t\t\t',llave)
+        llave = mover_bytes(llave)
+        print('DIFUSION LLAVE:\t\t', llave)
 
-    # Difusion 2
-    bloques_bytes[0] = difusion2(bloques_bytes[0])
-    print('DIFUSION 2:\t\t', bloques_bytes[0])
-    
-    #Confusion 2
-    bloques_bytes[0] = bytes(map(operator.xor, bloques_bytes[0], llave))
-    print('CONFUSION 2 XOR:\t\t', bloques_bytes[0])
-
+        # Difusion 2
+        bloques_bytes[i] = difusion2(bloques_bytes[i])
+        print('DIFUSION 2:\t\t', bloques_bytes[i])
+        
+        #Confusion 2
+        bloques_bytes[i] = bytes(map(operator.xor, bloques_bytes[i], llave))
+        print('CONFUSION XOR:\t\t', bloques_bytes[i])
+    print(bloques_bytes)
     print('#########################')
     # ------ DESCIFRADO ------
+    for i in reversed(range(len(bloques_bytes))):
+        print('Llave:\t\t\t',llave)
+        bloques_bytes[i] = bytes(map(operator.xor, bloques_bytes[i], llave))
+        print('DESCIFRADO - XOR:\t\t', bloques_bytes[i])
 
-    bloques_bytes[0] = bytes(map(operator.xor, bloques_bytes[0], llave))
-    print('DESCIFRADO - XOR:\t\t', bloques_bytes[0])
+        llave = mover_bytes_i(llave)
+        print('DESCIFRADO - DIFUSION LLAVE:\t',llave)
 
-    llave = mover_bytes_i(llave)
-    print('DESCIFRADO - DIFUSION LLAVE:\t',llave)
+        bloques_bytes[i] = desencriptar_difusion2(bloques_bytes[i])
+        print('DESCIFRADO - DIFUSION 2:\t',bloques_bytes[i])
 
-    bloques_bytes[0] = desencriptar_difusion2(bloques_bytes[0])
-    print('DESCIFRADO - DIFUSION 2:\t',bloques_bytes[0])
+        bloques_bytes[i] = cifrado_cesar(bloques_bytes[i], -1*get_desplazamiento(llave)).decode(CODE)
+        print('DESCIFRADO - CESAR:\t\t',bloques_bytes[i])
+    print(''.join(bloques_bytes)[:len_original])
+    
 
-    bloques_bytes[0] = cifrado_cesar(bloques_bytes[0], -1*get_desplazamiento(llave))
-    print('DESCIFRADO - CESAR:\t\t',bloques_bytes[0])
-
-   
+def agregar_x(texto):
+    mod_texto = len(texto) % TAMANO_BLOQUE
+    if mod_texto != 0:
+        dif = TAMANO_BLOQUE - mod_texto
+        # agregar x como dummy
+        texto += 'x'*dif  
+    return texto
 
 
 def desencriptar_difusion2(te):
     result = b''
     subbloques = dividir_bloques(te, int(TAMANO_BLOQUE/N_FILAS))
     for i in range(int(TAMANO_BLOQUE/N_FILAS)):
-        print(subbloques[0][i])
         result += bytes([subbloques[0][i]]) + bytes([subbloques[1][i]])
-    print(result)
     return result
 
 def difusion2(txt):
@@ -119,15 +127,11 @@ def mover_bytes_i(llave):
 def get_desplazamiento(llave):
     return extractKBits(llave[-1],3,1) + 1
 
-def dividir_bloques(texto, n):
-    mod_texto = len(texto) % n
-    if mod_texto != 0:
-        dif = n - mod_texto
-        # agregar x como dummy
-        texto += 'x'*dif          
+def dividir_bloques(texto, n):        
     return [texto[i:i+n] for i in range(0, len(texto), n)]
 
 def normalizar_llave(llave, n):
+    llave = agregar_x(llave)
     bloques = dividir_bloques(llave,n)
     for i in range(len(bloques)):
         bloques[i] = bytes(bloques[i], CODE)
